15b5893abde3af265da75a40eec2c4662535964b
chaitrali-shinde <chai.shinde100@gmail.com>
2020-06-08 23:22:00 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FrontPage from './components/FrontPage';
+
+function App() {
+  return (
+    <div className="App">
+      <FrontPage/>
+    </div>
+  );
+}
+
+export default App;
